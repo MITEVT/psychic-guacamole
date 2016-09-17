0b3d5077b0091801c6a2bbea4bac416cee10c548
Skanda Koppula <skoppula@mit.edu>
2016-09-17 10:27:05 -0400

    add solution

--- can/src/main.c
+++ can/src/main.c
@@ -1,7 +1,6 @@
 
 
 #include "chip.h"
-#include "util.h"
 #include <string.h>
 
 /*****************************************************************************
@@ -26,7 +25,7 @@ CCAN_MSG_OBJ_T msg_obj;
 STATIC RINGBUFF_T rx_buffer;
 CCAN_MSG_OBJ_T _rx_buffer[8];
 static bool can_error_flag;
-static bool can_error_info;
+static uint32_t can_error_info;
 
 static char str[100];
 static char uart_rx_buf[UART_RX_BUFFER_SIZE];
@@ -94,6 +93,7 @@ void CAN_rx(uint8_t msg_obj_num) {
 	/* Now load up the msg_obj structure with the CAN message */
 	LPC_CCAN_API->can_receive(&msg_obj);
 	if (msg_obj_num == 1) {
+                // TODO: Insert message object into ring buffer rx_buffer
 		RingBuffer_Insert(&rx_buffer, &msg_obj);
 	}
 }
@@ -102,9 +102,6 @@ void CAN_rx(uint8_t msg_obj_num) {
 /*	Function is executed by the Callback handler after
     a CAN message has been transmitted */
 void CAN_tx(uint8_t msg_obj_num) {
-	LED_Write(LED0, true);
-	LED_Write(LED1, true);
-	msg_obj_num = msg_obj_num;
 }
 
 /*	CAN error callback */
@@ -141,7 +138,7 @@ int main(void)
 	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_7, (IOCON_FUNC1 | IOCON_MODE_INACT));/* TXD */
 
 	Chip_UART_Init(LPC_USART);
-	Chip_UART_SetBaud(LPC_USART, 57600);
+	Chip_UART_SetBaud(LPC_USART, 9600);
 	Chip_UART_ConfigData(LPC_USART, (UART_LCR_WLEN8 | UART_LCR_SBS_1BIT | UART_LCR_PARITY_DIS));
 	Chip_UART_SetupFIFOS(LPC_USART, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2));
 	Chip_UART_TXEnable(LPC_USART);
@@ -151,6 +148,8 @@ int main(void)
 
 	LED_Init(LED0);
 	LED_Init(LED1);
+
+	LED_Write(LED0, true);
 
 	//---------------
 	//Ring Buffer
@@ -218,10 +217,15 @@ int main(void)
 
 		uint8_t count;
 		if ((count = Chip_UART_Read(LPC_USART, uart_rx_buf, UART_RX_BUFFER_SIZE)) != 0) {
+                        // when character 'a' is recieved, send a can message object with the following specs:
+                        //      - msgobj memory slot 2
+                        //      - CAN ID 0x700
+                        //      - one byte of data, 0xAA or whatever you choose
 			switch (uart_rx_buf[0]) {
 				case 'a':
 					DEBUG_Print("Sending CAN with ID: 0x600\r\n");
 					msg_obj.msgobj = 2;
+					msg_obj.mode_id = 0x600;
 					msg_obj.dlc = 1;
 					msg_obj.data[0] = 0xAA;
 					LPC_CCAN_API->can_transmit(&msg_obj);
